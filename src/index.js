38e07310a6c52866e0cbe0ae65a79e9de2c69855
Cristian Sanchez <ederson12f@gmail.com>
2022-03-08 19:26:54 -0500

    api entry point in express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+const express = require('express')
+const app = express()
+
+const { routerApi } = require('./routes')
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+routerApi(app)
+
+app.listen(PORT, () => {
+  console.log(`App listening in port: ${PORT}!`)
+})
