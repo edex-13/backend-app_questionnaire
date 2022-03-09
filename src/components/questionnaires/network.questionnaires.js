1021767d7d7f64a1c2de1a38cb33f55de1a97085
Cristian Sanchez <ederson12f@gmail.com>
2022-03-08 22:06:14 -0500

    finished component network layer , schemas to validation data with joi

--- src/components/questionnaires/network.questionnaires.js
+++ src/components/questionnaires/network.questionnaires.js
@@ -2,10 +2,94 @@ const expres = require('express')
 
 const router = expres.Router()
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world'
-  })
-})
+const validatorHandler = require('../../middlewares/validator.handle.js')
+const response = require('../../middlewares/respone.handle.js')
+const { createQuestionnaires, reciveAnswer, isUuid } = require('./schema.questionnaires.js')
+
+router.get('/',
+  async (req, res, next) => {
+    try {
+      res.locals.status = 200
+      res.locals.message = 'get all questionnaires'
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+  ,
+  response
+)
+
+router.post('/reply',
+  validatorHandler(reciveAnswer, 'body'),
+  async (req, res, next) => {
+    try {
+      res.locals.status = 200
+      res.locals.message = req.body
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+  ,
+  response
+)
+router.get('/reply/:id',
+  async (req, res, next) => {
+    try {
+      res.locals.status = 200
+      res.locals.message = req.params
+      next()
+    } catch (err) {
+      next(err)
+    }
+  },
+  response
+)
+
+router.get('/:id',
+  validatorHandler(isUuid, 'params'),
+  async (req, res, next) => {
+    try {
+      res.locals.status = 200
+      res.locals.message = req.params
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+  ,
+  response
+)
+
+router.post('/',
+  validatorHandler(createQuestionnaires, 'body'),
+  async (req, res, next) => {
+    try {
+      res.locals.status = 201
+      res.locals.message = req.body
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+  ,
+  response
+)
+
+router.delete('/:id',
+  validatorHandler(isUuid, 'params'),
+  async (req, res, next) => {
+    try {
+      res.locals.status = 200
+      res.locals.message = req.params
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+  ,
+  response
+)
 
 module.exports = router
