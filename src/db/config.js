3190b63a040213a90053350310e2db65cc179731
Cristian Sanchez <ederson12f@gmail.com>
2022-03-12 13:58:45 -0500

    basic settings for migrations

---
+++ src/db/config.js
@@ -0,0 +1,12 @@
+const url = process.env.DATABASE_URL || 'postgres://username:password@127.0.0.1:5432/myapp'
+
+module.exports = {
+  development: {
+    url,
+    dialect: 'postgres'
+  },
+  production: {
+    url,
+    dialect: 'postgres'
+  }
+}
